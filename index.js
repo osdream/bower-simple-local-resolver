fc48da339c3e21709435897bcba0e4edda19f33f
songao <songao@zenmen.com>
2017-11-24 09:48:42 +0800

    UPDATE: fix on releases

--- index.js
+++ index.js
@@ -11,12 +11,19 @@
  *
  **************************************************************************/
 
+var tmp = require('tmp');
+var copy = require('copy');
+var q = require('q');
+var path = require('path');
+
 /**
  * Factory function for resolver
  * It is called only one time by Bower, to instantiate resolver.
  * You can instantiate here any caches or create helper functions.
  */
 module.exports = function resolver (bower) {
+
+    bower.force = true;
 
   // Resolver factory returns an instance of resolver
   return {
@@ -30,28 +37,47 @@ module.exports = function resolver (bower) {
     // Optional:
     // Can resolve or normalize sources, like:
     // "jquery" => "git://github.com/jquery/jquery.git"
-    locate: function (source) {
-      return source;
-    },
+    // locate: function (source) {
+    //     return source;
+    // },
 
     // Optional:
     // Allows to list available versions of given source.
     // Bower chooses matching release and passes it to "fetch"
-    releases: function (source) {
-      return [
-        // always v1.0.0 ...
-        { target: 'v1.0.0', version: '1.0.0' }
-      ]
-    },
+    // releases: function (source) {
+    //   var deferred = q.defer();
+    //   md5Dir(source, function (err, hash) {
+    //     if (err) {
+    //         deferred.reject(new Error(err));
+    //     }
+    //     else {
+    //         deferred.resolve([
+    //           { target: hash, version: '0.0.1' }
+    //         ]);
+    //     }
+    //   });
+    //   return deferred.promise;
+    // },
 
     // It downloads package and extracts it to temporary directory
     // You can use npm's "tmp" package to tmp directories
     // See the "Resolver API" section for details on this method
-    fetch: function (endpoint) {
-      return {
-        tempPath: endpoint.source,
-        removeIgnores: true
-      }
+    fetch: function (endpoint, cached) {
+      var source = endpoint.source;
+      var deferred = q.defer();
+      var tempDir = tmp.dirSync();
+      copy(path.resolve(source, '**', '*'), tempDir.name, function (err, files) {
+        if (err) {
+            deferred.reject(new Error(err));
+        }
+        else {
+            deferred.resolve({
+              tempPath: tempDir.name,
+              removeIgnores: true
+            });
+        }
+      });
+      return deferred.promise;
     }
   }
 }
